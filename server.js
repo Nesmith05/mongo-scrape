40b52a07c09f56b846f921a2d53214ec93d3bdae
Thomas Greene <tecgreene93@gmail.com>
2018-11-04 15:29:41 -0500

    port fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 var exphbs = require("express-handlebars");
@@ -11,7 +11,7 @@ app.set("view engine", "handlebars");
 app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-console.log(MONGODB_URI);
+//console.log(MONGODB_URI);
 mongoose.connect(MONGODB_URI);
 
 app.use(require("./routes/routes.js"));
